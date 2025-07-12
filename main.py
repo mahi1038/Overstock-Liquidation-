4c466b65a2476e407790c8fc686504ffbe0ea958
mahi1038 <b23ee1038@iitj.ac.in>
2025-07-12 21:53:49 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
-from src.entity.config import DataIngestionConfig, TrainingConfig, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.entity.config import DataIngestionConfig, TrainingConfig, DataTransformationConfig, ModelTrainerConfig
 from datetime import datetime
 
 
@@ -15,4 +16,13 @@ if __name__ == '__main__':
     data_transformation_config = DataTransformationConfig(training_config)
     data_tansformation = DataTransformation(data_ingestion_artifact, data_transformation_config)
     data_transformation_artifact = data_tansformation.initiate_data_transformation()
+
+    model_trainer_config = ModelTrainerConfig(training_config)
+    model_trainer = ModelTrainer(data_transformation_artifact, model_trainer_config)
+    model_trainer_artifact = model_trainer.initiate_model_training()
+    print(model_trainer_artifact.test_metrics.rmsle_value)
+    print(model_trainer_artifact.test_metrics.smape_value)
+    print(model_trainer_artifact.train_metrics.rmsle_value)
+    print(model_trainer_artifact.train_metrics.smape_value)
+    
 
