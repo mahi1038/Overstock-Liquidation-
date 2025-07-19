e5c2a3596f36fbefd682e92f25fdee301ce695bf
mahi1038 <b23ee1038@iitj.ac.in>
2025-07-19 19:53:00 +0530

    another pipeline added

---
+++ main1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import os
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer_2 import ModelTrainer
+from src.entity.config import DataIngestionConfig, TrainingConfig, DataTransformationConfig, ModelTrainerConfig
+from datetime import datetime
+
+if __name__ == '__main__':
+    print("✅ Starting training pipeline")
+
+    training_config = TrainingConfig(datetime.now())
+    print("📦 Training config created")
+
+    data_ingestion_config = DataIngestionConfig(training_config)
+    data_ingestion = DataIngestion(data_ingestion_config)
+    data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+    sb_dataframe = pd.read_csv(data_ingestion_artifact.train_path)
+    print("📥 Data ingestion complete")
+
+    data_transformation_config = DataTransformationConfig(training_config)
+    data_transformation = DataTransformation(data_ingestion_artifact, data_transformation_config)
+    data_transformation_artifact = data_transformation.initiate_data_transformation()
+    print("🔄 Data transformation complete")
+
+    model_trainer_config = ModelTrainerConfig(training_config)
+    model_trainer = ModelTrainer(data_transformation_artifact, model_trainer_config)
+    model_trainer_artifact = model_trainer.initiate_model_training()
+    print("🤖 Model training complete")
+    # y_future = pd.read_csv(model_trainer_artifact.predicted_path)
+    #sb_dataframe.drop(columns = ['sales_28_sum'], axis = 1, inplace=True)
+    #sb_dataframe['future_sales'] = y_future
+
+
+
+    
